681b6827e853464daa84130de2d8515359f83e9a
DemLoveSky <dem.loginov@gmail.com>
2022-08-25 13:23:24 +0500

    Refactor v0.4.1

--- tools/dbCreater.js
+++ tools/dbCreater.js
@@ -2,10 +2,10 @@ const {readdirSync} = require('fs');
 
 module.exports = async (client) => {
     for (const dir of readdirSync("./commands/")) {
-        const tables = readdirSync(`./commands/${dir}/.db`).filter((file) => file.endsWith(".json"));
+        const tables = readdirSync(`./commands/${dir}/t_db`).filter((file) => file.endsWith(".json"));
 
         for (let file of tables) {
-            let cfgTable = require(`../commands/${dir}/.db/${file}`);
+            let cfgTable = require(`../commands/${dir}/t_db/${file}`);
             if (cfgTable.table && cfgTable.fields) {
                 const fields = cfgTable.fields.map(field => `${field.name} ${field.type} ${field.constraint}`).join();
                 await client.db.query(`CREATE TABLE IF NOT EXISTS ${cfgTable.table}(${fields});`)
